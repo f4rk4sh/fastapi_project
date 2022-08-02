2d820a01d762b8f39b406b656ff21338313cc76c
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-08-02 18:03:56 +0300

    making fixes in api/enrollment.py, core/config/db_config.py, main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,14 @@
 from fastapi import FastAPI, APIRouter, status
 
-from app.api.api_enrollment import api_router
+from app.api.api_enrollment import router
 from app.db import models
 from app.db.session import engine
 
-root_router = APIRouter()
 app = FastAPI(title="Fastapi project")
 
 models.Base.metadata.create_all(bind=engine)  # will be removed after adding flyway
 
-
-@root_router.get("/", status_code=status.HTTP_200_OK)
-def root() -> dict:
-    return {"Hello": "World!"}
-
-
-app.include_router(root_router)
-app.include_router(api_router)
+app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
