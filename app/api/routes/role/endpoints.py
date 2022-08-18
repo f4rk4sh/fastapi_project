1df1f94c656ad2949549108c976f01382c23da28
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-08-18 10:39:41 +0300

    adding documentation: changing GetCRUDDescriptions

--- app/api/routes/role/endpoints.py
+++ app/api/routes/role/endpoints.py
@@ -3,7 +3,7 @@ from fastapi.responses import Response
 from fastapi_utils.inferring_router import InferringRouter
 
 from app import crud
-from app.core.documentation.openapi_descriptions import get_crud_descriptions
+from app.core.documentation.openapi_descriptions import GetCRUDDescriptions
 from app.core.exceptions.exception_route_handler import ExceptionRouteHandler
 from app.core.documentation.openapi_tags import get_crud_tag_name
 from app.db.models import Role
@@ -11,41 +11,41 @@ from app.schemas.role import RoleResponse, RolesResponse, RoleSearchResults, Rol
     RoleDelete, RoleSearch
 
 router = InferringRouter(route_class=ExceptionRouteHandler, tags=[get_crud_tag_name(Role)])
-descriptions = get_crud_descriptions(model=Role, search_keywords=["name"])
+descriptions = GetCRUDDescriptions(model=Role, search_keywords=["name"])
 
 
-@router.get("/role", status_code=status.HTTP_200_OK, description=descriptions["fetch_all"])
+@router.get("/role", status_code=status.HTTP_200_OK, description=descriptions.fetch_all)
 def fetch_roles() -> RolesResponse:
     roles = crud.role.get_multi()
     return RolesResponse(roles=roles)
 
 
-@router.get("/role/search", status_code=status.HTTP_200_OK, description=descriptions["search"])
+@router.get("/role/search", status_code=status.HTTP_200_OK, description=descriptions.search)
 def search_roles(schema: RoleSearch = Depends()) -> RoleSearchResults:
     results = crud.role.search_by_name(role_name=schema.role_name, limit=schema.max_results)
     return RoleSearchResults(results=results)
 
 
-@router.post("/role", status_code=status.HTTP_201_CREATED, description=descriptions["create"])
+@router.post("/role", status_code=status.HTTP_201_CREATED, description=descriptions.create)
 def create_role(role_in: RoleCreate) -> RoleResponse:
     role = crud.role.create(obj_in=role_in)
     return RoleResponse.from_orm(role)
 
 
-@router.put("/role", status_code=status.HTTP_200_OK, description=descriptions["update"])
+@router.put("/role", status_code=status.HTTP_200_OK, description=descriptions.update)
 def update_role(recipe_in: RoleUpdate) -> RoleResponse:
     role = crud.role.get(id=recipe_in.id)
     updated_role = crud.role.update(db_obj=role, obj_in=recipe_in)
     return RoleResponse.from_orm(updated_role)
 
 
-@router.get("/role/{role_id}", status_code=status.HTTP_200_OK, description=descriptions["fetch_one"])
+@router.get("/role/{role_id}", status_code=status.HTTP_200_OK, description=descriptions.fetch_one)
 def fetch_role(schema: RoleGet = Depends()) -> RoleResponse:
     role = crud.role.get(id=schema.role_id)
     return RoleResponse.from_orm(role)
 
 
-@router.delete("/role/{role_id}", status_code=status.HTTP_204_NO_CONTENT, description=descriptions["delete"])
+@router.delete("/role/{role_id}", status_code=status.HTTP_204_NO_CONTENT, description=descriptions.delete)
 def delete_role(schema: RoleDelete = Depends()):
     crud.role.delete(id=schema.role_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/core/documentation/openapi_descriptions.py
+++ app/core/documentation/openapi_descriptions.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import List
 
 from app.db.models import Base
 
@@ -7,27 +7,11 @@ class GetCRUDDescriptions:
     """
     Get OpenAPI descriptions for basic CRUD operations endpoints
     """
-    @classmethod
-    def get_crud_descriptions(cls, model: Base, search_keywords: List[str]) -> Dict[str, str]:
+    def __init__(self, model: Base, search_keywords: List[str]):
         model_name = model.__name__.lower()
-        return {
-            "fetch_all":
-                f"""**Note:** fetch all {model_name}s from the database""",
-            "search":
-                f"""**Note:** search for {model_name}s in the database based on **{', '.join(search_keywords)}** keyword""",
-            "create":
-                f"""**Note:** create a new {model_name} in the database""",
-            "update":
-                f"""**Note:** update the {model_name} in the database""",
-            "fetch_one":
-                f""" **Note:** fetch a single {model_name} by **ID** from the database""",
-            "delete":
-                f"""**Note:** delete the {model_name} from the database"""
-
-        }
-
-    def __call__(self, model: Base,  search_keywords: List[str]):
-        return self.get_crud_descriptions(model, search_keywords)
-
-
-get_crud_descriptions: GetCRUDDescriptions = GetCRUDDescriptions()
+        self.fetch_all = f"""**Note:** fetch all {model_name}s from the database"""
+        self.search = f"""**Note:** search for {model_name}s in the database based on **{', '.join(search_keywords)}** keyword"""
+        self.create = f"""**Note:** create a new {model_name} in the database"""
+        self.update = f"""**Note:** update the {model_name} in the database"""
+        self.fetch_one = f""" **Note:** fetch a single {model_name} by **ID** from the database"""
+        self.delete = f"""**Note:** delete the {model_name} from the database"""
