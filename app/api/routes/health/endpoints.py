f36ecefe17bf1d6c02f1b41a9f8762f36393b426
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-09-19 14:09:18 +0300

    adding auth: fixing routes/health/endpoints.py

--- app/api/routes/health/endpoints.py
+++ app/api/routes/health/endpoints.py
@@ -1,9 +1,4 @@
-from fastapi import APIRouter, Response, status, Depends
-
-from app.api.dependencies import get_session
-from app.constansts.constants_role import ConstantRole
-from app.db.models import Session
-from app.security.permissions import permission
+from fastapi import APIRouter, Response, status
 
 router = APIRouter(tags=["health"])
 
@@ -11,6 +6,5 @@ router = APIRouter(tags=["health"])
 class HealthEndpoints:
     @staticmethod
     @router.get("/health/ping")
-    @permission({ConstantRole.employee})
-    def ping(session: Session = Depends(get_session)):
+    def ping():
         return Response(status_code=status.HTTP_200_OK, content="Pong")
