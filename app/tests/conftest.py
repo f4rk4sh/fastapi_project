1038212ec8a379508ec6893a0e0ea8af8d3b6ec8
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-09-22 18:47:44 +0300

    adding tests: adding function calls assertations in role crud tests, changing role crud tests to cbv

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,11 +3,15 @@ from typing import Generator
 import pytest
 from fastapi import FastAPI
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi.testclient import TestClient
 
 from app.api.api_enrollment import router
 from app.config.db_config import DBConfig
+from app.crud.crud_role import CRUDRole
 from app.db.base import Base
+from app.db.models import Role
+from app.manager.manager_role import RoleManager
 
 engine = create_engine(DBConfig.SQLALCHEMY_DATABASE_URL_TEST)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -22,9 +26,25 @@ def app() -> FastAPI:
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
-    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     yield TestingSessionLocal()
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture(scope="module")
+def client(app) -> Generator:
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def override_crud_role(db: Session):
+    return CRUDRole(Role, db)
+
+
+@pytest.fixture(scope="module")
+def override_manager_role(override_crud_role):
+    return RoleManager(Role, override_crud_role)
 
 
 
--- app/tests/role/test_role_crud.py
+++ app/tests/role/test_role_crud.py
@@ -1,72 +1,278 @@
+import pytest
+from pytest_mock import MockFixture
+from sqlalchemy.orm import Session
+
 from app.schemas.role import RoleCreate, RoleUpdate
 from app.tests.utils.base import random_string
 
 
-def test_create_role_from_schema(override_crud_role):
-    name = random_string()
-    role = override_crud_role.create(RoleCreate(name=name))
-    assert role
-    assert role.name == name
+class TestCreateRole:
+    def test_success_create_role_from_schema(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "create")
+
+        name = random_string()
+        role = override_crud_role.create(RoleCreate(name=name))
+
+        spy.assert_called_once()
+        assert role
+        assert role.name == name
+
+    def test_successful_create_role_from_dict(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "create")
+
+        name = random_string()
+        role = override_crud_role.create({"name": name})
+
+        spy.assert_called_once()
+        assert role
+        assert role.name == name
+
+    def test_successful_create_role_is_flush(
+            self,
+            override_crud_role,
+            db: Session,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "create")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()), is_flush=True)
+
+        db.rollback()
+
+        roles_in_db = override_crud_role.get_multi()
+
+        spy.assert_called_once()
+        assert role not in roles_in_db
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_create_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "create")
+
+        role = override_crud_role.create({"name": random_string(51)})
+        role_in_db = override_crud_role.get(role.id)
+
+        spy.assert_called_once()
+        assert not role_in_db
 
 
-def test_create_role_from_dict(override_crud_role):
-    name = random_string()
-    role = override_crud_role.create({"name": name})
-    assert role
-    assert role.name == name
+class TestGetRole:
+    def test_successful_get_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get(role.id)
+
+        spy.assert_called_once()
+        assert role_in_db
+        assert role_in_db.id == role.id
+        assert role_in_db.name == role.name
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_get_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get(role.name)
+
+        spy.assert_called_once()
+        assert not role_in_db
 
 
-def test_get_role(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    role_in_db = override_crud_role.get(role.id)
-    assert role_in_db
-    assert role_in_db.id == role.id
-    assert role_in_db.name == role.name
+class TestGetMultipleRoles:
+    def test_successful_get_multiple_roles(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get_multi")
+
+        roles = [override_crud_role.create(RoleCreate(name=random_string())) for _ in range(3)]
+        roles_in_db = override_crud_role.get_multi()
+
+        spy.assert_called_once()
+        assert roles_in_db
+        for role in roles:
+            assert role in roles_in_db
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_get_multiple_roles(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get_multi")
+
+        roles = [override_crud_role.create(RoleCreate(name=random_string())) for _ in range(3)]
+        roles_in_db = override_crud_role.get_multi(limit=-1)
+
+        spy.assert_called_once()
+        assert not roles_in_db
+        for role in roles:
+            assert role not in roles_in_db
 
 
-def test_get_multiple_roles(override_crud_role):
-    roles = [override_crud_role.create(RoleCreate(name=random_string())) for _ in range(3)]
-    roles_in_db = override_crud_role.get_multi()
-    assert roles_in_db
-    for role in roles:
+class TestGetRoleByAttribute:
+    def test_successful_get_role_by_attribute(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get_by_attribute")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get_by_attribute(name=role.name)
+
+        spy.assert_called_once()
+        assert role_in_db
+        assert role_in_db.id == role.id
+        assert role_in_db.name == role.name
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_get_role_by_attribute(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "get_by_attribute")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get_by_attribute(name=role.id)
+
+        spy.assert_called_once()
+        assert not role_in_db
+
+
+class TestSearchRoleByParameter:
+    def test_successful_search_roles_by_parameter(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "search_by_parameter")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        roles_in_db = override_crud_role.search_by_parameter(parameter="name", keyword=role.name)
+
+        spy.assert_called_once()
+        assert roles_in_db
         assert role in roles_in_db
 
+    @pytest.mark.xfail(strict=True)
+    def test_failed_search_roles_by_parameter(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "search_by_parameter")
 
-def test_get_role_by_attribute(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    role_in_db = override_crud_role.get_by_attribute(name=role.name)
-    assert role_in_db
-    assert role_in_db.id == role.id
-    assert role_in_db.name == role.name
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        roles_in_db = override_crud_role.search_by_parameter(parameter=None, keyword=role.name)
 
-
-def test_search_roles_by_parameter(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    roles_in_db = override_crud_role.search_by_parameter(parameter="name", keyword=role.name)
-    assert roles_in_db
-    assert role in roles_in_db
-
-
-def test_update_role_from_schema(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    role_in_db = override_crud_role.get(role.id)
-    new_name = random_string()
-    updated_role = override_crud_role.update(role_in_db, RoleUpdate(id=role.id, name=new_name))
-    assert updated_role
-    assert updated_role.name == new_name
+        spy.assert_called_once()
+        assert not roles_in_db
 
 
-def test_update_role_from_dict(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    role_in_db = override_crud_role.get(role.id)
-    new_name = random_string()
-    updated_role = override_crud_role.update(role_in_db, {"id": role.id, "name": new_name})
-    assert updated_role
-    assert updated_role.name == new_name
+class TestUpdateRole:
+    def test_successful_update_role_from_schema(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "update")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get(role.id)
+
+        new_name = random_string()
+        updated_role = override_crud_role.update(role_in_db, RoleUpdate(id=role.id, name=new_name))
+
+        spy.assert_called_once()
+        assert updated_role
+        assert updated_role.name == new_name
+
+    def test_successful_update_role_from_dict(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "update")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get(role.id)
+
+        new_name = random_string()
+        updated_role = override_crud_role.update(role_in_db, {"id": role.id, "name": new_name})
+
+        spy.assert_called_once()
+        assert updated_role
+        assert updated_role.name == new_name
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_update_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "update")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        role_in_db = override_crud_role.get(role.id)
+
+        new_name = random_string()
+        override_crud_role.update(role_in_db, RoleUpdate(id=role.id, new_name=new_name))
+
+        role_in_db = override_crud_role.get(role.id)
+
+        spy.assert_called_once()
+        assert role_in_db.name != new_name
 
 
-def test_delete_role(override_crud_role):
-    role = override_crud_role.create(RoleCreate(name=random_string()))
-    override_crud_role.delete(role.id)
-    role_in_db = override_crud_role.get_by_attribute(id=role.id)
-    assert not role_in_db
+class TestDeleteRole:
+    def test_successful_delete_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "delete")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        override_crud_role.delete(role.id)
+        role_in_db = override_crud_role.get_by_attribute(id=role.id)
+
+        spy.assert_called_once()
+        assert not role_in_db
+
+    @pytest.mark.xfail(strict=True)
+    def test_failed_delete_role(
+            self,
+            override_crud_role,
+            mocker: MockFixture
+    ) -> None:
+        spy = mocker.spy(override_crud_role, "delete")
+
+        role = override_crud_role.create(RoleCreate(name=random_string()))
+        override_crud_role.delete(role.id)
+        role_in_db = override_crud_role.get_by_attribute(id=role.name)
+
+        spy.assert_called_once()
+        assert role_in_db
