d9ff1d345d048153ed091cd09c0aa7eb5858126d
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-10 14:09:26 +0300

    adding database models: adding flyway migrations

---
+++ deployment/migrations/versions/U3__Add_payment_method_bank_account_type_payment_payment_status_tables.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS [db].[dbo].[payment];
+GO
+
+DROP TABLE IF EXISTS [db].[dbo].[payment_method];
+GO
+
+DROP TABLE IF EXISTS [db].[dbo].[bank];
+GO
+
+DROP TABLE IF EXISTS [db].[dbo].[account_type];
+GO
+
+DROP TABLE IF EXISTS [db].[dbo].[payment_status];
+GO
