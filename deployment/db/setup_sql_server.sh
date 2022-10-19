a60c0f2bc8ab4950677b78615b726392892daf09
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-19 13:38:43 +0300

    fixing setup_sql_server.sh

--- deployment/db/setup_sql_server.sh
+++ deployment/db/setup_sql_server.sh
@@ -2,7 +2,7 @@ echo "=== Waiting for MSSQL Server starting up... ==="
 
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S db -U sa -P password123! -d master -i /src/deployment/db/setup_sql_server.sql
+    /opt/mssql-tools/bin/sqlcmd -S "${DB_HOST}" -U "${DB_USER}" -P "${MSSQL_SA_PASSWORD}" -d master -i /src/deployment/db/setup_sql_server.sql
     if [ $? -eq 0 ]
     then
         echo "=== MSSQL Server starting up completed ==="
