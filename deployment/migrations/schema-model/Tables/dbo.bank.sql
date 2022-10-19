c399b130fd5b105cc3f23e6d7989dcdde7349873
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-19 13:52:21 +0300

    adding database models: changing flyway schema-model tables, adding employee_account schema-model table

--- deployment/migrations/schema-model/Tables/dbo.bank.sql
+++ deployment/migrations/schema-model/Tables/dbo.bank.sql
@@ -2,11 +2,10 @@ CREATE TABLE [dbo].[bank]
 (
 [id] [int] NOT NULL IDENTITY(1, 1),
 [name] [varchar] (50) NULL,
-[edrpou] [varchar] (50) NULL,
 [mfo] [varchar] (50) NULL,
-[iban] [varchar] (50) NULL,
-[card] [varchar] (50) NULL,
-[account_type_id] [int] NULL
+[is_active] [BIT] NULL DEFAULT 0,
+[creation_date] [datetime] NULL,
+[deactivation_date] [datetime] NULL
 )
 GO
 
@@ -14,7 +13,4 @@ ALTER TABLE [dbo].[bank] ADD CONSTRAINT [PK__bank__id] PRIMARY KEY CLUSTERED ([i
 GO
 
 CREATE NONCLUSTERED INDEX [ix_bank_id] ON [dbo].[bank] ([id])
-GO
-
-ALTER TABLE [dbo].[bank] ADD CONSTRAINT [FK__bank__account_type__id] FOREIGN KEY ([account_type_id]) REFERENCES [dbo].[account_type] ([id]) ON DELETE CASCADE
 GO
---
+++ deployment/migrations/schema-model/Tables/dbo.employer_payment_method.sql
@@ -0,0 +1,23 @@
+CREATE TABLE [dbo].[employer_payment_method]
+(
+[id] [int] NOT NULL IDENTITY(1, 1),
+[iban] [varchar] (50) NULL,
+[is_active] [BIT] NULL DEFAULT 0,
+[creation_date] [datetime] NULL,
+[deactivation_date] [datetime] NULL,
+[employer_id] [int] NULL,
+[bank_id] [int] NULL
+)
+GO
+
+ALTER TABLE [dbo].[employer_payment_method] ADD CONSTRAINT [PK__employer_payment_method__id] PRIMARY KEY CLUSTERED ([id])
+GO
+
+CREATE NONCLUSTERED INDEX [ix_employer_payment_method_id] ON [dbo].[employer_payment_method] ([id])
+GO
+
+ALTER TABLE [dbo].[employer_payment_method] ADD CONSTRAINT [FK__employer_payment_method__employer__id] FOREIGN KEY ([employer_id]) REFERENCES [dbo].[employer] ([id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[employer_payment_method] ADD CONSTRAINT [FK__employer_payment_method__bank__id] FOREIGN KEY ([bank_id]) REFERENCES [dbo].[bank] ([id]) ON DELETE CASCADE
+GO
