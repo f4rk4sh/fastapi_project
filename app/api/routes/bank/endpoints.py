c9fd87b6071f8620fbebb8c5c4dd1bef5d0f8d6a
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-24 15:58:29 +0300

    minor changes in bank/endpoints.py

--- app/api/routes/bank/endpoints.py
+++ app/api/routes/bank/endpoints.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from fastapi import status, Depends
+from fastapi import Depends, status
 from fastapi_utils.inferring_router import InferringRouter
 from pydantic import PositiveInt
 
@@ -10,12 +10,14 @@ from app.api.docs.api_params import CRUDParamsDescriptions
 from app.constansts.constants_role import ConstantRole
 from app.db.models import Session
 from app.manager.manager_bank import bank
-from app.schemas.schema_bank import BankResponse, BankCreate, BankUpdate
+from app.schemas.schema_bank import BankCreate, BankResponse, BankUpdate
 from app.security.permissions import permission
 from app.utils.exceptions.exception_route_handler import ExceptionRouteHandler
 
 router = InferringRouter(route_class=ExceptionRouteHandler, tags=["Banks"])
-descriptions = CRUDEndpointsDescriptions(model_name="Bank", search_parameters=["name", "mfo"])
+descriptions = CRUDEndpointsDescriptions(
+    model_name="Bank", search_parameters=["name", "mfo"]
+)
 parameters = CRUDParamsDescriptions(obj_name="Bank")
 
 
@@ -31,7 +33,7 @@ def search_banks(
     parameter: str = parameters.search_parameter,
     keyword: str = parameters.search_keyword,
     max_results: Optional[PositiveInt] = parameters.max_results_search,
-    session: Session = Depends(get_session)
+    session: Session = Depends(get_session),
 ) -> List[BankResponse]:
     return bank.search(parameter, keyword, session, max_results)
 
@@ -54,11 +56,15 @@ def update_bank(
     return bank.update(bank_in, session)
 
 
-@router.get("/bank/{bank_id}", status_code=status.HTTP_200_OK, description=descriptions.fetch_one)
+@router.get(
+    "/bank/{bank_id}",
+    status_code=status.HTTP_200_OK,
+    description=descriptions.fetch_one,
+)
 @permission({ConstantRole.employer})
 def fetch_bank(
     bank_id: PositiveInt = parameters.get_id,
-    session: Session = Depends(get_session)
+    session: Session = Depends(get_session),
 ) -> BankResponse:
     return bank.fetch_one(bank_id, session)
 
@@ -67,6 +73,6 @@ def fetch_bank(
 @permission({ConstantRole.employer})
 def delete_bank(
     bank_id: PositiveInt = parameters.delete_id,
-    session: Session = Depends(get_session)
+    session: Session = Depends(get_session),
 ):
     return bank.delete(bank_id, session)
