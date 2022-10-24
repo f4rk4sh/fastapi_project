1990d903d59d9aa29741c9b9e8e5972e730480d9
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-24 17:58:49 +0300

    adding payment status type schemas, crud, manager

---
+++ app/manager/manager_payment_status_type.py
@@ -0,0 +1,20 @@
+from app.crud.crud_payment_status_type import CRUDPaymentStatusType
+from app.crud.crud_payment_status_type import \
+    payment_status_type as crud_payment_status_type
+from app.db.models import PaymentStatusType
+from app.manager.manager_base import ManagerBase
+from app.schemas.schema_payment_status_type import PaymentStatusTypeCreate, PaymentStatusTypeUpdate
+
+
+class PaymentStatusTypeManager(
+    ManagerBase[
+        PaymentStatusType,
+        CRUDPaymentStatusType,
+        PaymentStatusTypeCreate,
+        PaymentStatusTypeUpdate,
+    ]
+):
+    pass
+
+
+payment_status_type: PaymentStatusTypeManager = PaymentStatusTypeManager(crud_payment_status_type)
