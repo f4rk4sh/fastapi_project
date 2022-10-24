27af8ff7aeb5a4fc7fd48a61df48b6d0da98a530
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-24 17:21:06 +0300

    adding account type manager

---
+++ app/manager/manager_account_type.py
@@ -0,0 +1,11 @@
+from app.crud.crud_account_type import CRUDAccountType, account_type as crud_account_type
+from app.db.models import AccountType
+from app.manager.manager_base import ManagerBase
+from app.schemas.schema_account_type import AccountTypeCreate, AccountTypeUpdate
+
+
+class AccountTypeManager(ManagerBase[AccountType, CRUDAccountType, AccountTypeCreate, AccountTypeUpdate]):
+    pass
+
+
+account_type = AccountTypeManager(crud_account_type)
