2c429403851e49cd51708113292ce2e0222333be
Yaroslav Farkash <farkashyaroslav@gmail.com>
2022-10-25 13:39:52 +0300

    adding employer payment method endpoints

--- app/api/api_enrollment.py
+++ app/api/api_enrollment.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.routes.auth.endpoints import router as auth_router
 from app.api.routes.employee.endpoints import router as employee_router
 from app.api.routes.employer.endpoints import router as employer_router
+from app.api.routes.employer_payment_method.endpoints import router as employer_payment_method_router
 from app.api.routes.employer_type.endpoints import router as employer_type_router
 from app.api.routes.health.endpoints import router as health_router
 from app.api.routes.role.endpoints import router as role_router
@@ -12,6 +13,7 @@ router = APIRouter()
 router.include_router(auth_router)
 router.include_router(employee_router)
 router.include_router(employer_router)
+router.include_router(employer_payment_method_router)
 router.include_router(health_router)
 router.include_router(role_router)
 router.include_router(status_type_router)
